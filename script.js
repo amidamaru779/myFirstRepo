f86cd98e394cbffc97582392f2d0283e2415f778
Nikita Polev <nikitko779@gmail.com>
2024-08-02 19:21:53 +0300

    вывел в консоль типы данных значений переменных

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let title = "My first project on Java Script."
-let screens = 1
-let screenPrice = 1 
-let rollback = 1
-let fullPrice = 1
-let adaptive = 1
+// строка с названием проекта
+let screens = "Простые, Сложные, Интерактивные"
+// строка с названием экранов
+let screenPrice = 12
+// любое число
+let rollback = 95
+// любое число от 1 до 100
+let fullPrice = 150
+// сколько бы хотел зарабатывать
+let adaptive = true
+// булевое значение
 
-console.log();
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
