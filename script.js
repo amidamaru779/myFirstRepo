9298c4c35854c999380692d8125566ac15fdee27
Nikita Polev <nikitko779@gmail.com>
2024-08-05 20:26:44 +0300

    добавил условия для расчета скидки

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // задаём вопросы пользователю о проекте
 let title = prompt("Как называется ваш проект?")
 
-let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные?")
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные")
 
-let screenPrice = prompt("Сколько будет стоить данная работа?",  "12000")
+let screenPrice = +prompt("Сколько будет стоить данная работа?",  "12000")
 
 let adaptive = confirm("Нужен ли адаптив на сайте?")
 
 let service1 = prompt("Какой дополнительный тип услуги нужен?")
 
-let servicePrice1 = prompt("Сколько это будет стоить?")
+let servicePrice1 = +prompt("Сколько это будет стоить?")
 
 let service2 = prompt("Какой дополнительный тип услуги нужен?")
 
-let servicePrice2 = prompt("Сколько это будет стоить?")
+let servicePrice2 = +prompt("Сколько это будет стоить?")
 
 //высчитываем полную стоимость
-let total = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2)
+let total = screenPrice + servicePrice1 + servicePrice2
 
 let rollback = 95
 
@@ -26,6 +26,17 @@ console.log(fullPrice);
 
 let servicePercentPrice = Math.ceil(fullPrice - (rollback/100))
 console.log((servicePercentPrice));
+
+//создал условия для проверки процента скидки
+if (fullPrice > 30000) {
+    console.log("Даем скидку в 10%");
+} else if (15000 <= fullPrice && fullPrice <= 30000) {
+    console.log("Даем скидку в 5%");
+} else if (15000 > fullPrice && fullPrice > 0) {
+    console.log("Скидка не предусмотрена");
+} else if (fullPrice <= 0) {
+    console.log("Что-то пошло не так");
+}
 
 
 
