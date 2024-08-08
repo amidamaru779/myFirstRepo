fa40528ed2ff306703fafbcd3b50cf7c1840fe8c
Nikita Polev <nikitko779@gmail.com>
2024-08-08 22:28:20 +0300

    выполнил задание

--- script.js
+++ script.js
@@ -10,53 +10,57 @@ let servicePrice1 = +prompt("Сколько это будет стоить?")
 let service2 = prompt("Какой дополнительный тип услуги нужен?")
 let servicePrice2 = +prompt("Сколько это будет стоить?")
 let rollback = 95
-let fullPrice = screenPrice + servicePrice1 + servicePrice2
-let servicePercentPrice = Math.ceil(fullPrice - (rollback/100))
-
-//создал условия для проверки процента скидки
-if (fullPrice > 30000) {
-    console.log("Даем скидку в 10%");
-} else if (15000 <= fullPrice && fullPrice <= 30000) {
-    console.log("Даем скидку в 5%");
-} else if (15000 > fullPrice && fullPrice > 0) {
-    console.log("Скидка не предусмотрена");
-} else if (fullPrice <= 0) {
-    console.log("Что-то пошло не так");
-}
-
-console.log(fullPrice);
-console.log((servicePercentPrice));
-function getAllServicePrices(params) {
+let fullPrice 
+let servicePercentPrice  
+let allServicePrices
+const showTypeOf = function(variable) {
+    console.log(variable, typeof variable);
     
 }
 
 
+const getAllServicePrices = function() {
+    return servicePrice1 + servicePrice2
+}
 
+function getFullPrice() {
+    return screenPrice + allServicePrices 
+     
+}
 
+const getRollbackMessage = function(price) {
+    if (price > 30000) {
+        return "Даем скидку в 10%"
+    } else if (15000 <= price && price <= 30000) {
+        return "Даем скидку в 5%"
+    } else if (15000 > price && price > 0) {
+        return "Скидка не предусмотрена"
+    } else if (price <= 0) {
+        return "Что-то пошло не так"
+    }
+}
 
+const  getTitle = function() {
+    title = title.trim();
+    title = title.charAt(0).toUpperCase() + title.slice(1).toLowerCase();
+    return title
+}
 
+function getServicePercentPrices() {
+    return Math.ceil(fullPrice - (rollback/100))
 
+}
 
+allServicePrices = getAllServicePrices()
+fullPrice = getFullPrice()
+getTitle()
+servicePercentPrice = getServicePercentPrices()
 
-let fullPrice = 150
-
-let adaptive = true
-
-console.log(typeof title);
-
-console.log(typeof fullPrice);
-
-console.log(typeof adaptive);
-
-console.log(screens.length);
-
-console.log("Стоимость верстки экранов", screenPrice, "рублей/долларов/гривен/юани \n"+"Стоимость разработки сайта",fullPrice,"рублей/долларов/гривен/юани"
- ); 
-screens = screens.toLowerCase()
-
-screens = screens.split(", ")
-
+showTypeOf(title)
+showTypeOf(fullPrice)
+showTypeOf(adaptive)
 console.log(screens);
 
-console.log(fullPrice * (rollback/100));
-
+console.log(getRollbackMessage(fullPrice));
+console.log("Стоимость верстки экранов", screenPrice, "рублей/долларов/гривен/юани \n"+"Стоимость разработки сайта",fullPrice,"рублей/долларов/гривен/юани"); 
+ 
